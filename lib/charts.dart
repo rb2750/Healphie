39fe819f6f1a57573400dd3021a2bfd33464e28d
Rob <rb2750@live.co.uk>
2019-10-27 20:41:37 +0000

    Storage added, charts completed. Data just needs to be inserted into the charts now.

--- lib/charts.dart
+++ lib/charts.dart
@@ -1,59 +1,154 @@
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 
-class TimeVsHappinessBarChart extends StatelessWidget {
-  final List<charts.Series> seriesList;
-  final bool animate;
+class Charts extends StatefulWidget {
+  @override
+  ChartState createState() => ChartState();
+}
 
-  TimeVsHappinessBarChart(this.seriesList, {this.animate});
+class ChartState extends State<Charts> {
+  final bool animate = true;
+  TimeScale chartTimeScale = TimeScale.DAY;
+  ChartType chartType = ChartType.BAR;
 
-  factory TimeVsHappinessBarChart.withSampleData() {
-    return new TimeVsHappinessBarChart(
-      createData(),
-      animate: false,
+  Future<List<charts.Series<OrdinalHappinessBar, String>>> _barData;
+  Future<List<charts.Series<OrdinalHappinessPie, String>>> _pieData;
+
+  @override
+  void initState() {
+    super.initState();
+    _barData = createBarData();
+    _pieData = createPieData();
+  }
+
+  Widget makeButton(String text, Border border, TimeScale newTimeScale, ChartType newChartType, bool selected) {
+    return InkWell(
+//      focusColor: Colors.black,
+//      highlightColor: Colors.black,
+//      hoverColor: Colors.black,
+      onTap: () {
+        setState(() {
+          chartTimeScale = newTimeScale;
+          chartType = newChartType;
+        });
+      },
+      child: new Container(
+        //width: 100.0,
+        height: 50.0, padding: EdgeInsets.all(10),
+        decoration: new BoxDecoration(border: border, color: selected ? Colors.black12 : Colors.white),
+        child: new Center(
+            child: Text(
+          text,
+          style: TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.w400),
+        )),
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return new charts.BarChart(seriesList,
-        animate: animate,
-        vertical: false,
-        primaryMeasureAxis: new charts.NumericAxisSpec(tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 1), renderSpec: new charts.NoneRenderSpec()));
+    Widget chartWidget;
+
+    BorderSide border = BorderSide(color: Colors.black12, width: 1.0);
+    if (chartType == ChartType.BAR) {
+      chartWidget = FutureBuilder<List<charts.Series<OrdinalHappinessBar, String>>>(
+          initialData: new List<charts.Series<OrdinalHappinessBar, String>>(),
+          future: _barData,
+          builder: (BuildContext context, AsyncSnapshot<List<charts.Series<OrdinalHappinessBar, String>>> snapshot) {
+            Widget chart;
+            chart = charts.BarChart(snapshot.data, animate: animate, vertical: false, primaryMeasureAxis: new charts.NumericAxisSpec(tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 1), renderSpec: new charts.NoneRenderSpec()));
+            return chart;
+          });
+    } else {
+      chartWidget = FutureBuilder<List<charts.Series<OrdinalHappinessPie, String>>>(
+          initialData: new List<charts.Series<OrdinalHappinessPie, String>>(),
+          future: _pieData,
+          builder: (BuildContext context, AsyncSnapshot<List<charts.Series<OrdinalHappinessPie, String>>> snapshot) {
+            return charts.PieChart(snapshot.data, animate: animate, defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [new charts.ArcLabelDecorator(labelPosition: charts.ArcLabelPosition.outside)]));
+          });
+    }
+    return LayoutBuilder(builder: (BuildContext context, BoxConstraints viewportConstraints) {
+      return Container(
+          child: Column(children: <Widget>[
+        Container(
+            decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12))),
+            child: Row(children: <Widget>[
+              makeButton("Day", Border(right: border), TimeScale.DAY, chartType, chartTimeScale == TimeScale.DAY),
+              makeButton("Month", Border(right: border), TimeScale.MONTH, chartType, chartTimeScale == TimeScale.MONTH),
+              makeButton("Year", Border(right: border), TimeScale.YEAR, chartType, chartTimeScale == TimeScale.YEAR),
+              Spacer(),
+              makeButton("Pie", Border(left: border), chartTimeScale, ChartType.PIE, chartType == ChartType.PIE),
+              makeButton("Bar", Border(left: border), chartTimeScale, ChartType.BAR, chartType == ChartType.BAR)
+            ])),
+        Container(height: viewportConstraints.maxHeight - 51, child: chartWidget)
+      ]));
+    });
   }
 
-  static List<charts.Series<OrdinalHappiness, String>> createData() {
+  Future<List<charts.Series<OrdinalHappinessBar, String>>> createBarData() async {
+    List<String> relevantData = new List<String>();
+
     final data = [
-      new OrdinalHappiness('09:00', charts.Color.fromHex(code: "#ff0000")),
-      new OrdinalHappiness('10:00', charts.Color.fromHex(code: "#ff0000")),
-      new OrdinalHappiness('11:00', charts.Color.fromHex(code: "#ff0000")),
-      new OrdinalHappiness('12:00', charts.Color.fromHex(code: "#ff0000")),
-      new OrdinalHappiness('13:00', charts.Color.fromHex(code: "#00ff00")),
-      new OrdinalHappiness('14:00', charts.Color.fromHex(code: "#ff00ff")),
-      new OrdinalHappiness('15:00', charts.Color.fromHex(code: "#ff00ff")),
-      new OrdinalHappiness('16:00', charts.Color.fromHex(code: "#ff00ff")),
-      new OrdinalHappiness('17:00', charts.Color.fromHex(code: "#00ff00")),
-      new OrdinalHappiness('18:00', charts.Color.fromHex(code: "#00ff00")),
-      new OrdinalHappiness('19:00', charts.Color.fromHex(code: "#ff0000")),
-      new OrdinalHappiness('20:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('09:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('10:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('11:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('12:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('13:00', charts.Color.fromHex(code: "#00ff00")),
+      new OrdinalHappinessBar('14:00', charts.Color.fromHex(code: "#ff00ff")),
+      new OrdinalHappinessBar('15:00', charts.Color.fromHex(code: "#ff00ff")),
+      new OrdinalHappinessBar('16:00', charts.Color.fromHex(code: "#ff00ff")),
+      new OrdinalHappinessBar('17:00', charts.Color.fromHex(code: "#00ff00")),
+      new OrdinalHappinessBar('18:00', charts.Color.fromHex(code: "#00ff00")),
+      new OrdinalHappinessBar('19:00', charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessBar('20:00', charts.Color.fromHex(code: "#ff0000")),
     ];
 
     return [
-      new charts.Series<OrdinalHappiness, String>(
+      new charts.Series<OrdinalHappinessBar, String>(
         id: 'Happiness',
-        domainFn: (OrdinalHappiness sales, _) => sales.year,
-        measureFn: (OrdinalHappiness sales, _) => 1,
-        fillColorFn: (OrdinalHappiness sales, _) => sales.color,
+        domainFn: (OrdinalHappinessBar sales, _) => sales.year,
+        measureFn: (OrdinalHappinessBar sales, _) => 1,
+        fillColorFn: (OrdinalHappinessBar sales, _) => sales.color,
         data: data,
+      )
+    ];
+  }
+
+  Future<List<charts.Series<OrdinalHappinessPie, String>>> createPieData() async {
+    final data = [
+      new OrdinalHappinessPie('Happy', 5, charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessPie('Sad', 50, charts.Color.fromHex(code: "#ff0000")),
+      new OrdinalHappinessPie('Anxious', 50, charts.Color.fromHex(code: "#ff0000")),
+    ];
+    return [
+      new charts.Series<OrdinalHappinessPie, String>(
+        id: 'HappinessPie',
+        data: data,
+        domainFn: (OrdinalHappinessPie data, _) => data.emotion,
+        measureFn: (OrdinalHappinessPie data, _) => data.size,
+        fillColorFn: (OrdinalHappinessPie data, _) => data.color,
+        labelAccessorFn: (OrdinalHappinessPie data, _) => data.emotion,
       )
     ];
   }
 }
 
-class OrdinalHappiness {
+enum TimeScale { DAY, MONTH, YEAR }
+
+enum ChartType { PIE, BAR }
+
+class OrdinalHappinessBar {
   final String year;
   final charts.Color color;
 
-  OrdinalHappiness(this.year, this.color);
+  OrdinalHappinessBar(this.year, this.color);
+}
+
+class OrdinalHappinessPie {
+  final String emotion;
+  final int size;
+  final charts.Color color;
+
+  OrdinalHappinessPie(this.emotion, this.size, this.color);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'charts.dart';
 
 void main() => runApp(MyApp());
@@ -27,7 +31,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String selected;
 
-  Widget body= LogHealthFragment();
+  Widget body;
+
+  _MyHomePageState() {
+    body = LogHealthFragment(this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     ]))),
             Expanded(
                 child: ListView(
-                  padding: EdgeInsets.only(top: 10),
+              padding: EdgeInsets.only(top: 10),
               children: <Widget>[
                 Column(
                   children: <Widget>[
@@ -68,7 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       onTap: () {
                         Navigator.pop(context);
                         setState(() {
-                          body = LogHealthFragment();
+                          body = LogHealthFragment(this);
                         });
                       },
                     ),
@@ -84,8 +92,9 @@ class _MyHomePageState extends State<MyHomePage> {
                       ),
                       onTap: () {
                         Navigator.pop(context);
+//                        showCharts();
                         setState(() {
-                          body = TimeVsHappinessBarChart.withSampleData();
+                          body = Charts();
                         });
                       },
                     )
@@ -102,16 +111,239 @@ class _MyHomePageState extends State<MyHomePage> {
                     ))))
           ],
         )),
-        appBar: AppBar(elevation: 2, iconTheme: new IconThemeData(color: Colors.white), title: Text("Sophie Health")),
+        appBar: AppBar(
+          elevation: 2,
+          iconTheme: new IconThemeData(color: Colors.white),
+          title: Text("Sophie Health"),
+        ),
         body: body);
+  }
+
+  void showCharts() {
+    setState(() {
+      body = Charts();
+    });
   }
 }
 
 class LogHealthFragment extends StatelessWidget {
+  int happyColor = 0xFFFAB6F8;
+  int relaxedColor = 0xFFAEDFE8;
+  int boredColor = 0xFFAEE8C4;
+  int veryBadColor = 0xFFE80909;
+  int anxiousColor = 0xFF9F92F0;
+  int numbColor = 0xFFED72F0;
+  final _MyHomePageState pageState;
+
+  LogHealthFragment(this.pageState);
+
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text("Hello!"),
-    );
+    return new Container(
+        padding: EdgeInsets.only(left: 20, top: 20),
+        child: Column(
+          children: <Widget>[
+            Align(child: Text("How are you feeling?", style: TextStyle(fontSize: 30))),
+            Padding(
+                padding: EdgeInsets.fromLTRB(10, 30, 10, 20),
+                child: Wrap(
+                  spacing: 60, runSpacing: 30,
+//                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(happyColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.favorite,
+                                color: Colors.white,
+                              ),
+                              iconSize: 110,
+                              padding: EdgeInsets.only(top: 10),
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), happyColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Happy",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(relaxedColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.star,
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.zero,
+                              iconSize: 110,
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), relaxedColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Relaxed",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(boredColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.extension,
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.only(left: 10, bottom: 10),
+                              iconSize: 110,
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), boredColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Bored",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(veryBadColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.delete,
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.zero,
+                              iconSize: 110,
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), veryBadColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Very Bad",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(anxiousColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.visibility,
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.zero,
+                              iconSize: 110,
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), anxiousColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Anxious",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        new Container(
+                            width: 130,
+                            height: 130,
+                            decoration: new BoxDecoration(
+                              color: Color(numbColor),
+                              shape: BoxShape.circle,
+                            ),
+                            child: IconButton(
+                              icon: Icon(
+                                Icons.face,
+                                color: Colors.white,
+                              ),
+                              padding: EdgeInsets.zero,
+                              iconSize: 110,
+                              onPressed: () async {
+                                final prefs = await SharedPreferences.getInstance();
+                                prefs.setInt(new DateFormat("dd-MM-yyyy hh:mm:ss").format(DateTime.now()), numbColor);
+                                pageState.showCharts();
+                              },
+                            )),
+                        Text(
+                          "Numb",
+                          style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                        )
+                      ],
+                    ),
+                  ],
+                )),
+          ],
+        ));
+  }
+}
+
+class CustomBoxShadow extends BoxShadow {
+  final BlurStyle blurStyle;
+
+  const CustomBoxShadow({
+    Color color = const Color(0xFF000000),
+    Offset offset = Offset.zero,
+    double blurRadius = 0.0,
+    this.blurStyle = BlurStyle.normal,
+  }) : super(color: color, offset: offset, blurRadius: blurRadius);
+
+  @override
+  Paint toPaint() {
+    final Paint result = Paint()
+      ..color = color
+      ..maskFilter = MaskFilter.blur(this.blurStyle, blurSigma);
+    assert(() {
+      if (debugDisableShadows) result.maskFilter = null;
+      return true;
+    }());
+    return result;
   }
 }
