9a61227cdb2b2003d4a80d88fc31967f3f819ef0
Rob <rob@bladen.me>
2020-05-25 16:10:12 +0100

    Increase pie text size and make it black.

--- src/charts.dart
+++ src/charts.dart
@@ -34,7 +34,8 @@ class ChartState extends State<Charts> {
     _pieData = createPieData();
   }
 
-  Widget makeButton(String text, Border border, TimeScale newTimeScale, ChartType newChartType, bool selected) {
+  Widget makeButton(String text, Border border, TimeScale newTimeScale,
+      ChartType newChartType, bool selected) {
     return InkWell(
       onTap: () {
         if (chartTimeScale != newTimeScale || newChartType != chartType) {
@@ -47,11 +48,13 @@ class ChartState extends State<Charts> {
       child: new Container(
         //width: 100.0,
         height: 50.0, padding: EdgeInsets.all(10),
-        decoration: new BoxDecoration(border: border, color: selected ? Colors.black12 : Colors.white),
+        decoration: new BoxDecoration(
+            border: border, color: selected ? Colors.black12 : Colors.white),
         child: new Center(
             child: Text(
           text,
-          style: TextStyle(fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.w400),
+          style: TextStyle(
+              fontSize: 20.0, color: Colors.black, fontWeight: FontWeight.w400),
         )),
       ),
     );
@@ -63,45 +66,72 @@ class ChartState extends State<Charts> {
 
     BorderSide border = BorderSide(color: Colors.black12, width: 1.0);
     if (chartType == ChartType.BAR) {
-      chartWidget = FutureBuilder<List<charts.Series<OrdinalHappinessBar, String>>>(
+      chartWidget = FutureBuilder<
+              List<charts.Series<OrdinalHappinessBar, String>>>(
           initialData: new List<charts.Series<OrdinalHappinessBar, String>>(),
           future: _barData,
-          builder: (BuildContext context, AsyncSnapshot<List<charts.Series<OrdinalHappinessBar, String>>> snapshot) {
+          builder: (BuildContext context,
+              AsyncSnapshot<List<charts.Series<OrdinalHappinessBar, String>>>
+                  snapshot) {
             Widget chart;
             chart = charts.BarChart(snapshot.data,
                 animate: animate,
                 vertical: false,
-                primaryMeasureAxis: new charts.NumericAxisSpec(tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 1), renderSpec: new charts.NoneRenderSpec()));
+                primaryMeasureAxis: new charts.NumericAxisSpec(
+                    tickProviderSpec: new charts.BasicNumericTickProviderSpec(
+                        desiredTickCount: 1),
+                    renderSpec: new charts.NoneRenderSpec()));
             return chart;
           });
     } else {
-      chartWidget = FutureBuilder<List<charts.Series<OrdinalHappinessPie, String>>>(
+      chartWidget = FutureBuilder<
+              List<charts.Series<OrdinalHappinessPie, String>>>(
           initialData: new List<charts.Series<OrdinalHappinessPie, String>>(),
           future: _pieData,
-          builder: (BuildContext context, AsyncSnapshot<List<charts.Series<OrdinalHappinessPie, String>>> snapshot) {
+          builder: (BuildContext context,
+              AsyncSnapshot<List<charts.Series<OrdinalHappinessPie, String>>>
+                  snapshot) {
             return charts.PieChart(snapshot.data,
-                animate: animate, defaultRenderer: new charts.ArcRendererConfig(arcRendererDecorators: [new charts.ArcLabelDecorator(labelPosition: charts.ArcLabelPosition.inside)]));
+                animate: animate,
+                defaultRenderer:
+                    new charts.ArcRendererConfig(arcRendererDecorators: [
+                  new charts.ArcLabelDecorator(
+                      labelPosition: charts.ArcLabelPosition.inside,
+                      insideLabelStyleSpec:
+                          new charts.TextStyleSpec(color: charts.MaterialPalette.black,fontSize: 18))
+                ]));
           });
     }
-    return LayoutBuilder(builder: (BuildContext context, BoxConstraints viewportConstraints) {
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
       return Container(
           child: Column(children: <Widget>[
         Container(
-            decoration: BoxDecoration(border: Border(bottom: BorderSide(color: Colors.black12))),
+            decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.black12))),
             child: Row(children: <Widget>[
-              makeButton("Day", Border(right: border), TimeScale.DAY, chartType, chartTimeScale == TimeScale.DAY),
-              makeButton("Month", Border(right: border), TimeScale.MONTH, chartType, chartTimeScale == TimeScale.MONTH),
-              makeButton("Year", Border(right: border), TimeScale.YEAR, chartType, chartTimeScale == TimeScale.YEAR),
+              makeButton("Day", Border(right: border), TimeScale.DAY, chartType,
+                  chartTimeScale == TimeScale.DAY),
+              makeButton("Month", Border(right: border), TimeScale.MONTH,
+                  chartType, chartTimeScale == TimeScale.MONTH),
+              makeButton("Year", Border(right: border), TimeScale.YEAR,
+                  chartType, chartTimeScale == TimeScale.YEAR),
               Spacer(),
-              makeButton("Pie", Border(left: border), chartTimeScale, ChartType.PIE, chartType == ChartType.PIE),
-              makeButton("Bar", Border(left: border), chartTimeScale, ChartType.BAR, chartType == ChartType.BAR)
+              makeButton("Pie", Border(left: border), chartTimeScale,
+                  ChartType.PIE, chartType == ChartType.PIE),
+              makeButton("Bar", Border(left: border), chartTimeScale,
+                  ChartType.BAR, chartType == ChartType.BAR)
             ])),
-        Container(height: viewportConstraints.maxHeight - 51, child: chartWidget, padding:EdgeInsets.all(10))
+        Container(
+            height: viewportConstraints.maxHeight - 51,
+            child: chartWidget,
+            padding: EdgeInsets.all(10))
       ]));
     });
   }
 
-  Future<List<charts.Series<OrdinalHappinessBar, String>>> createBarData() async {
+  Future<List<charts.Series<OrdinalHappinessBar, String>>>
+      createBarData() async {
     List<OrdinalHappinessBar> data = new List<OrdinalHappinessBar>();
 
     SharedPreferences prefs = await SharedPreferences.getInstance();
@@ -110,26 +140,31 @@ class ChartState extends State<Charts> {
       bool relevant = false;
 
       if (chartTimeScale == TimeScale.DAY) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(hours: 12)));
+        relevant = !DateTime.now()
+            .isBefore(datetime.subtract(new Duration(hours: 12)));
       } else if (chartTimeScale == TimeScale.MONTH) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(days: 30)));
+        relevant =
+            !DateTime.now().isBefore(datetime.subtract(new Duration(days: 30)));
       } else if (chartTimeScale == TimeScale.YEAR) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(days: 365)));
+        relevant = !DateTime.now()
+            .isBefore(datetime.subtract(new Duration(days: 365)));
       }
 
       if (relevant) {
         Color color = Color(prefs.getInt(key));
-        charts.Color chartColor = charts.Color(r: color.red, g: color.green, b: color.blue);
+        charts.Color chartColor =
+            charts.Color(r: color.red, g: color.green, b: color.blue);
         data.add(new OrdinalHappinessBar(datetime, chartColor));
       }
     }
 
-    data.sort((a,b)=>b.date.compareTo(a.date));
+    data.sort((a, b) => b.date.compareTo(a.date));
 
     return [
       new charts.Series<OrdinalHappinessBar, String>(
         id: 'Happiness',
-        domainFn: (OrdinalHappinessBar sales, _) => new DateFormat("HH:mm").format(sales.date),
+        domainFn: (OrdinalHappinessBar sales, _) =>
+            new DateFormat("HH:mm").format(sales.date),
         measureFn: (OrdinalHappinessBar sales, _) => 1,
         fillColorFn: (OrdinalHappinessBar sales, _) => sales.color,
         data: data,
@@ -137,7 +172,8 @@ class ChartState extends State<Charts> {
     ];
   }
 
-  Future<List<charts.Series<OrdinalHappinessPie, String>>> createPieData() async {
+  Future<List<charts.Series<OrdinalHappinessPie, String>>>
+      createPieData() async {
     List<OrdinalHappinessPie> data = new List<OrdinalHappinessPie>();
     HashMap<int, int> dataCount = new HashMap<int, int>();
 
@@ -147,15 +183,19 @@ class ChartState extends State<Charts> {
       bool relevant = false;
 
       if (chartTimeScale == TimeScale.DAY) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(days: 1)));
+        relevant =
+            !DateTime.now().isBefore(datetime.subtract(new Duration(days: 1)));
       } else if (chartTimeScale == TimeScale.MONTH) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(days: 30)));
+        relevant =
+            !DateTime.now().isBefore(datetime.subtract(new Duration(days: 30)));
       } else if (chartTimeScale == TimeScale.YEAR) {
-        relevant = !DateTime.now().isBefore(datetime.subtract(new Duration(days: 365)));
+        relevant = !DateTime.now()
+            .isBefore(datetime.subtract(new Duration(days: 365)));
       }
 
       if (relevant) {
-        dataCount.update(prefs.getInt(key), (value) => ++value, ifAbsent: () => 1);
+        dataCount.update(prefs.getInt(key), (value) => ++value,
+            ifAbsent: () => 1);
       }
     }
 
@@ -168,10 +208,11 @@ class ChartState extends State<Charts> {
       if (entry.key == EmotionColors.anxiousColor) emotion = "Anxious";
       if (entry.key == EmotionColors.numbColor) emotion = "Numb";
       Color color = Color(entry.key);
-      charts.Color chartColor = charts.Color(r: color.red, g: color.green, b: color.blue);
+      charts.Color chartColor =
+          charts.Color(r: color.red, g: color.green, b: color.blue);
       data.add(new OrdinalHappinessPie(emotion, entry.value, chartColor));
     }
-    
+
     return [
       new charts.Series<OrdinalHappinessPie, String>(
         id: 'HappinessPie',
